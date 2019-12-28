0c1c9c01e7bb66d2acd2226033051dd983abe69c
maoki maoki <maoki.ikoam@gmail.com>
2019-12-29 01:31:35 +0900

    change message

--- GitPlacticeApp/GitPlacticeApp/Program.cs
+++ GitPlacticeApp/GitPlacticeApp/Program.cs
@@ -6,7 +6,7 @@ namespace GitPlacticeApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello sunday!");
         }
     }
 }
