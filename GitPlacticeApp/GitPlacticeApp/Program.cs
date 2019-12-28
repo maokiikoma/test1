69787b006e7725522f030ffb2453af2fc44857f7
maoki maoki <maoki.ikoam@gmail.com>
2019-12-29 02:11:45 +0900

    wk2 message

--- GitPlacticeApp/GitPlacticeApp/Program.cs
+++ GitPlacticeApp/GitPlacticeApp/Program.cs
@@ -7,6 +7,9 @@ namespace GitPlacticeApp
         static void Main(string[] args)
         {
             Console.WriteLine("Hollo sunday!");
+
+            var hoge = "test";
+            Console.WriteLine(hoge);
         }
     }
 }
