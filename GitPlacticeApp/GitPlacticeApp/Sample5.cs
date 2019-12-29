320a0a3f25180e9d7c50ac7dd42e4a36c16125cf
maoki maoki <maoki.ikoam@gmail.com>
2019-12-29 11:52:49 +0900

    add sample5

---
+++ GitPlacticeApp/GitPlacticeApp/Sample5.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GitPlacticeApp
+{
+    class Sample5
+    {
+        public int Item1 { get; set; }
+        public int Item2 { get; set; }
+        public int Item3 { get; set; }
+    }
+}
